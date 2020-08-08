b931b0c0c2fdd8fa28ad0cbc2cdf165ad38db4b2
royb3 <roy.buitenhuis94@gmail.com>
2020-08-08 13:41:41 +0200

    Add Scroll frame to options panel and create a list like view.  (+Tex)

--- main.lua
+++ main.lua
@@ -9,10 +9,9 @@ local zoneShardID = nil
 local addonChannelName = "AZP-IT-AC"
 local OptionsPanel
 
-local foodItemData = AIU.foodItemData
-local flaskItemData = AIU.flaskItemData
+local itemData = AIU.itemData
 local initialConfig = AIU.initialConfig
-
+local testItemData = AIU.itemData["Flasks"]
 local addonVersion = "v0.1"
 local dash = " - "
 local name = "InstanceUtility"
@@ -51,7 +50,7 @@ end
 -- end
 
 function addonMain:OnLoad(self)
-    addonMain:createParentFrames(flaskItemData)
+    --- addonMain:createParentFrames(flaskItemData)
 
     local InstanceUtilityAddonFrame = CreateFrame("FRAME", "InstanceUtilityAddonFrame", UIParent)
     InstanceUtilityAddonFrame:SetPoint("CENTER", 0, 0)
@@ -88,9 +87,36 @@ function addonMain:OnLoad(self)
     TempTestButton1.contentText:SetPoint("CENTER", 0, -1)
     TempTestButton1:SetScript("OnClick", function() addonMain:checkListButtonClicked() end )
 
+
+    ReloadButton = CreateFrame("Button", "ReloadButton", InstanceUtilityAddonFrame, "UIPanelButtonTemplate")
+    ReloadButton.contentText = ReloadButton:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
+    ReloadButton.contentText:SetText("Reload!")
+    ReloadButton:SetWidth("100")
+    ReloadButton:SetHeight("25")
+    ReloadButton.contentText:SetWidth("100")
+    ReloadButton.contentText:SetHeight("15")
+    ReloadButton:SetPoint("TOP", 100, -50)
+    ReloadButton.contentText:SetPoint("CENTER", 0, -1)
+    ReloadButton:SetScript("OnClick", function() ReloadUI(); end )
+    
     OptionsPanel = CreateFrame("FRAME", "AZP-IU-OptionsPanel")
     OptionsPanel.name = "AzerPUG InstanceUtility"
     InterfaceOptions_AddCategory(OptionsPanel)
+
+    OpenSettingsButton = CreateFrame("Button", "OpenSettingsButton", InstanceUtilityAddonFrame, "UIPanelButtonTemplate")
+    OpenSettingsButton.contentText = OpenSettingsButton:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
+    OpenSettingsButton.contentText:SetText("Open Options!")
+    OpenSettingsButton:SetWidth("100")
+    OpenSettingsButton:SetHeight("25")
+    OpenSettingsButton.contentText:SetWidth("100")
+    OpenSettingsButton.contentText:SetHeight("15")
+    OpenSettingsButton:SetPoint("TOP", 100, -75)
+    OpenSettingsButton.contentText:SetPoint("CENTER", 0, -1)
+    OpenSettingsButton:SetScript("OnClick", function() InterfaceOptionsFrame_OpenToCategory(OptionsPanel); InterfaceOptionsFrame_OpenToCategory(OptionsPanel); end )
+
+    
+
+
 
     local OptionsHeader = OptionsPanel:CreateFontString("OptionsHeader", "ARTWORK", "GameFontNormalHuge")
     OptionsHeader:SetText(promo .. dash .. "Options")
@@ -99,93 +125,19 @@ function addonMain:OnLoad(self)
     OptionsHeader:SetPoint("TOP", 0, -10)
 end
 
-function addonMain:initConfigSection(dataSet, iOffset)
-    for i,itemIDs in ipairs(dataSet) do
-        for j,itemID in ipairs(itemIDs) do
-            local itemName, _, _, _, _, _, _, _, _, itemIcon = GetItemInfo(itemID)
-            local itemCheckBox = CreateFrame("CheckButton", "itemCheckBox", OptionsPanel, "ChatConfigCheckButtonTemplate")
-            itemCheckBox:SetPoint("TOPLEFT", 150 * j - 150, -25 * (i + iOffset) - 50)
-            itemCheckBox:SetChecked(AIUCheckedData["checkItemIDs"][itemID])
-            itemCheckBox:SetScript("OnClick", function()
-                if itemCheckBox:GetChecked() == true then
-                    AIUCheckedData["checkItemIDs"][itemID] = true
-                elseif itemCheckBox:GetChecked() == false then
-                    AIUCheckedData["checkItemIDs"][itemID] = nil
-                end
-            end)
-
-            local itemIconLabel = CreateFrame("Frame", "itemIconLabel", OptionsPanel)
-            itemIconLabel:SetSize(15,15)
-            itemIconLabel:SetPoint("TOPLEFT", 150 * j - 125, -25 * (i + iOffset) - 55)
-            itemIconLabel.texture = itemIconLabel:CreateTexture(nil, "BACKGROUND")
-            itemIconLabel.texture:SetPoint("CENTER")
-            itemIconLabel.texture:SetTexture(itemIcon)
-            itemIconLabel.texture:SetSize(15,15)
-
-            local itemNameLabel = CreateFrame("Frame", "itemNameLabel", OptionsPanel)
-            itemNameLabel:SetSize(104, 10)
-            itemNameLabel:SetPoint("TOPLEFT", 150 * j - 125, -25 * (i + iOffset) - 55)
-            itemNameLabel.contentText = itemNameLabel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-            itemNameLabel.contentText:SetPoint("CENTER")
-            itemNameLabel.contentText:SetText(itemName)
-            itemNameLabel.contentText:SetSize(104, 10)
-
-            itemCheckBox:SetScript("OnEnter", function()
-                GameTooltip:SetOwner(itemCheckBox)
-                GameTooltip:SetItemByID(itemID)
-                GameTooltip:Show()
-            end)
-            itemCheckBox:SetScript("OnLeave", function() GameTooltip:Hide() end)
-        end
-    end
+function addonMain:initConfigSection()
+    addonMain:createTreeGroupList();
 end
+
+
 
 function addonMain:initializeConfig()
     if AIUCheckedData == nil then
         AIUCheckedData = initialConfig
     end
 
-    addonMain:initConfigSection(flaskItemData, 0)
-    addonMain:initConfigSection(foodItemData, #flaskItemData + 1)
-
-    -- for i,itemIDs in ipairs(flaskItemData) do
-    --     for j,itemID in ipairs(itemIDs) do
-    --         local itemName, _, _, _, _, _, _, _, _, itemIcon = GetItemInfo(itemID)
-    --         local itemCheckBox = CreateFrame("CheckButton", "itemCheckBox", OptionsPanel, "ChatConfigCheckButtonTemplate")
-    --         itemCheckBox:SetPoint("TOPLEFT", 150 * j - 150, -25 * i - 50)
-    --         itemCheckBox:SetChecked(AIUCheckedData["checkItemIDs"][itemID])
-    --         itemCheckBox:SetScript("OnClick", function()
-    --             if itemCheckBox:GetChecked() == true then
-    --                 AIUCheckedData["checkItemIDs"][itemID] = true
-    --             elseif itemCheckBox:GetChecked() == false then
-    --                 AIUCheckedData["checkItemIDs"][itemID] = nil
-    --             end
-    --         end)
-
-    --         local itemIconLabel = CreateFrame("Frame", "itemIconLabel", OptionsPanel)
-    --         itemIconLabel:SetSize(15,15)
-    --         itemIconLabel:SetPoint("TOPLEFT", 150 * j - 125, -25 * i - 55)
-    --         itemIconLabel.texture = itemIconLabel:CreateTexture(nil, "BACKGROUND")
-    --         itemIconLabel.texture:SetPoint("CENTER")
-    --         itemIconLabel.texture:SetTexture(itemIcon)
-    --         itemIconLabel.texture:SetSize(15,15)
-
-    --         local itemNameLabel = CreateFrame("Frame", "itemNameLabel", OptionsPanel)
-    --         itemNameLabel:SetSize(104, 10)
-    --         itemNameLabel:SetPoint("TOPLEFT", 150 * j - 125, -25 * i - 55)
-    --         itemNameLabel.contentText = itemNameLabel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    --         itemNameLabel.contentText:SetPoint("CENTER")
-    --         itemNameLabel.contentText:SetText(itemName)
-    --         itemNameLabel.contentText:SetSize(104, 10)
-
-    --         itemCheckBox:SetScript("OnEnter", function()
-    --             GameTooltip:SetOwner(itemCheckBox)
-    --             GameTooltip:SetItemByID(itemID)
-    --             GameTooltip:Show()
-    --         end)
-    --         itemCheckBox:SetScript("OnLeave", function() GameTooltip:Hide() end)
-    --     end
-    -- end
+    addonMain:initConfigSection()
+    
 end
 
 function addonMain:OnEvent(self, event, ...)
@@ -238,6 +190,123 @@ function addonMain:createParentFrames(dataSet)
                 GameTooltip:Show()
             end)
             itemNameLabel:SetScript("OnLeave", function() GameTooltip:Hide() end)
+        end
+    end
+end
+
+function addonMain:createTreeGroupList()
+    -- local treeGroupList = createFrame("Frame", "treeGroupList", OptionsPanel);
+    -- treeGroupList:SetPoint("TOPLEFT", 5, 5)
+    -- treeGroupList:SetSize(OptionsPanel::);
+    local scrollFrame = CreateFrame("ScrollFrame", "scrollFrame", OptionsPanel, "UIPanelScrollFrameTemplate");
+    --scrollFrame:SetSize(OptionsPanel:GetWidth(), OptionsPanel:GetHeight())
+    scrollFrame:SetSize(500, 450)
+    scrollFrame:SetPoint("TOPLEFT", 0, -100)
+    scrollFrame:SetBackdropColor(0, 1, 1, 0.5);
+    -- local itemFlaskData = itemData["Flasks"]
+    local itemFlaskData = itemData["Flasks"]
+    
+    local flaskFrame = CreateFrame("Frame", "flaskFrame", scrollFrame);
+    flaskFrame:SetPoint("TOPLEFT", scrollFrame, "TOPLEFT");
+    flaskFrame:SetSize(500, 100)
+        local lastFrame = nil
+        for j,itemSection in ipairs(itemFlaskData) do
+
+            local sectionFrame = CreateFrame("Frame", "sectionFrame", flaskFrame)
+            if lastFrame == nil then
+                sectionFrame:SetPoint("TOPLEFT", 0, 0)
+            else
+                sectionFrame:SetPoint("TOPLEFT", lastFrame, "BOTTOMLEFT", 0, 0)
+            end
+            sectionFrame:SetSize(400, 100)
+            lastFrame = sectionFrame
+            sectionFrame.contentText = sectionFrame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+            sectionFrame.contentText:SetPoint("TOPLEFT", 10, 5)
+            sectionFrame.contentText:SetText(itemSection[1])
+             
+        for i,itemID in ipairs(itemSection[2]) do
+            
+            local itemName, _, _, _, _, _, _, _, _, itemIcon = GetItemInfo(itemID)
+            local parentFrame = CreateFrame("Frame", "parentFrame", sectionFrame)
+            parentFrame:SetSize(300,20)
+            parentFrame:SetPoint("TOPLEFT", 25, -10 + i * -15)
+
+            local itemCheckBox = CreateFrame("CheckButton", "itemCheckBox", parentFrame, "ChatConfigCheckButtonTemplate")
+            itemCheckBox:SetSize(20, 20)
+            itemCheckBox:SetPoint("LEFT", 5, 0)
+            itemCheckBox:SetChecked(AIUCheckedData["checkItemIDs"][itemID])
+            itemCheckBox:SetScript("OnClick", function()
+                if itemCheckBox:GetChecked() == true then
+                    AIUCheckedData["checkItemIDs"][itemID] = true
+                elseif itemCheckBox:GetChecked() == false then
+                    AIUCheckedData["checkItemIDs"][itemID] = nil
+                end
+            end)
+
+            local itemIconLabel = CreateFrame("Frame", "checkIcon", parentFrame)
+            itemIconLabel:SetSize(15, 15)
+            itemIconLabel:SetPoint("TOPLEFT", 35, 0)
+            itemIconLabel.texture = itemIconLabel:CreateTexture(nil, "BACKGROUND")
+            itemIconLabel.texture:SetPoint("LEFT", 0, 0)
+            itemIconLabel.texture:SetTexture(itemIcon)
+            itemIconLabel.texture:SetSize(15, 15)
+
+            local itemNameLabel = CreateFrame("Frame", "itemNameLabel", parentFrame)
+            itemNameLabel:SetSize(175, 10)
+            itemNameLabel:SetPoint("TOPLEFT", 55, -2)
+            itemNameLabel.contentText = itemNameLabel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+            itemNameLabel.contentText:SetPoint("LEFT", 0, 0)
+            itemNameLabel.contentText:SetText(itemName)
+
+            itemIconLabel:SetScript("OnEnter", function()
+                GameTooltip:SetOwner(itemIconLabel)
+                GameTooltip:SetItemByID(itemID)
+                GameTooltip:SetSize(200, 200)
+                GameTooltip:Show()
+            end)
+            itemIconLabel:SetScript("OnLeave", function() GameTooltip:Hide() end)
+
+            itemNameLabel:SetScript("OnEnter", function()
+                GameTooltip:SetOwner(itemNameLabel)
+                GameTooltip:SetItemByID(itemID)
+                GameTooltip:SetSize(200, 200)
+                GameTooltip:Show()
+            end)
+            itemNameLabel:SetScript("OnLeave", function() GameTooltip:Hide() end)
+            -- local itemName, _, _, _, _, _, _, _, _, itemIcon = GetItemInfo(itemID)
+            -- local itemCheckBox = CreateFrame("CheckButton", "itemCheckBox", OptionsPanel, "ChatConfigCheckButtonTemplate")
+            -- itemCheckBox:SetPoint("TOPLEFT", 150 * j - 150, -25 * (i + iOffset) - 50)
+            -- itemCheckBox:SetChecked(AIUCheckedData["checkItemIDs"][itemID])
+            -- itemCheckBox:SetScript("OnClick", function()
+            --     if itemCheckBox:GetChecked() == true then
+            --         AIUCheckedData["checkItemIDs"][itemID] = true
+            --     elseif itemCheckBox:GetChecked() == false then
+            --         AIUCheckedData["checkItemIDs"][itemID] = nil
+            --     end
+            -- end)
+
+            -- local itemIconLabel = CreateFrame("Frame", "itemIconLabel", OptionsPanel)
+            -- itemIconLabel:SetSize(15,15)
+            -- itemIconLabel:SetPoint("TOPLEFT", 150 * j - 125, -25 * (i + iOffset) - 55)
+            -- itemIconLabel.texture = itemIconLabel:CreateTexture(nil, "BACKGROUND")
+            -- itemIconLabel.texture:SetPoint("CENTER")
+            -- itemIconLabel.texture:SetTexture(itemIcon)
+            -- itemIconLabel.texture:SetSize(15,15)
+
+            -- local itemNameLabel = CreateFrame("Frame", "itemNameLabel", OptionsPanel)
+            -- itemNameLabel:SetSize(104, 10)
+            -- itemNameLabel:SetPoint("TOPLEFT", 150 * j - 125, -25 * (i + iOffset) - 55)
+            -- itemNameLabel.contentText = itemNameLabel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+            -- itemNameLabel.contentText:SetPoint("LEFT")
+            -- itemNameLabel.contentText:SetText(itemName)
+            -- itemNameLabel.contentText:SetSize(104, 10)
+
+            -- itemCheckBox:SetScript("OnEnter", function()
+            --     GameTooltip:SetOwner(itemCheckBox)
+            --     GameTooltip:SetItemByID(itemID)
+            --     GameTooltip:Show()
+            -- end)
+            -- itemCheckBox:SetScript("OnLeave", function() GameTooltip:Hide() end)
         end
     end
 end
