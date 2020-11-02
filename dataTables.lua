b51067a2237a48a473c814f9c33c7f9cc03df34b
Arco Dielhof <arcodielhof@hotmail.com>
2020-11-02 18:50:51 +0100

    Update coming with AZP-IU-CheckList-v15

--- dataTables.lua
+++ dataTables.lua
@@ -5,7 +5,7 @@ AIU.ModuleStats =
 {
     ["Versions"] =
     {
-        ["CheckList"] = 14,
+        ["CheckList"] = 15,
         ["ReadyCheck"] = 13,
         ["InstanceLeading"] = 8,
     },
--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local OnEvent = AZP.IU.OnEvent
 
 local initialConfig = AIU.initialConfig
 
-local AZPIUCoreVersion = 27
+local AZPIUCoreVersion = 28
 local dash = " - "
 local name = "InstanceUtility" .. dash .. "Core"
 local nameFull = "AzerPUG " .. name
