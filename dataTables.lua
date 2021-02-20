037a5c58a3f47951d2afc040786dab026a70c9d6
Arco Dielhof <arcodielhof@hotmail.com>
2021-02-20 16:24:01 +0100

    Update supporting AZP-IU-ReadyCheck-v25.

--- dataTables.lua
+++ dataTables.lua
@@ -6,7 +6,7 @@ AIU.ModuleStats =
     ["Versions"] =
     {
         ["CheckList"] = 23,
-        ["ReadyCheck"] = 24,
+        ["ReadyCheck"] = 25,
         ["InstanceLeading"] = 17,
         ["GreatVault"] = 3,
         ["ManaGement"] = 6,
--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local OnEvent = AZP.IU.OnEvent
 
 local initialConfig = AIU.initialConfig
 
-local AZPIUCoreVersion = 57
+local AZPIUCoreVersion = 58
 local dash = " - "
 local name = "InstanceUtility" .. dash .. "Core"
 local nameFull = "AzerPUG " .. name
