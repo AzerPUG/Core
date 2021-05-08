a72d94fb31194d6b1ec3cbde1d3afba306ab9f1f
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2021-05-09 01:21:41 +0200

    Update supporting Interrupt Helper V8

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 72
+AZP.VersionControl["Core"] = 73
 if AZP.Core == nil then AZP.Core = {} end
 
 local dash = " - "
