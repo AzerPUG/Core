488a5a21d4fc6b84053d3f61653a7cb94746501b
Arco Dielhof <arcodielhof@hotmail.com>
2021-05-10 17:03:10 +0200

    Added promo panels to options for all addons. Made generic option panels a bit more generic.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 74
+AZP.VersionControl["Core"] = 75
 if AZP.Core == nil then AZP.Core = {} end
 
 local dash = " - "
--- optionsPanels.lua
+++ optionsPanels.lua
@@ -3,7 +3,7 @@ AZP.OptionsPanels = {}
 local OptionsPromoPanels = nil
 local CopyPasteEditBoxFrame = nil
 
-function AZP.OptionsPanels:Generic(panelName, panelTitle, panelContent)
+function AZP.OptionsPanels:Generic(panelName, panelContent)
 	local optionFrame = CreateFrame("FRAME")
 	optionFrame:SetSize(500, 500)
 	-- optionFrame:SetPoint("CENTER", 0, 0)
@@ -14,18 +14,43 @@ function AZP.OptionsPanels:Generic(panelName, panelTitle, panelContent)
 	optionFrame.title = optionFrame:CreateFontString(nil, "ARTWORK", "GameFontNormalHuge")
 	optionFrame.title:SetSize(optionFrame:GetWidth(), 50)
 	optionFrame.title:SetPoint("TOP")
-	optionFrame.title:SetText(panelTitle)
+	
+
+	OptionsPromoPanels = CreateFrame("Frame", nil, optionFrame)
+    OptionsPromoPanels:SetSize(500, 100)
+    OptionsPromoPanels:SetPoint("TOP", 0, 25)
+
+	local c1, c2, c3, c4 = -192, -64, 64, 192
+	local r1, r2 = -45, -90
+
+	AZP.OptionsPanels:CreatePromoPanels(c1, r1, AZP.Core.Media["Website"])
+	AZP.OptionsPanels:CreatePromoPanels(c2, r1, AZP.Core.Media["Discord"])
+	AZP.OptionsPanels:CreatePromoPanels(c3, r1, AZP.Core.Media["CurseForge"])
+	AZP.OptionsPanels:CreatePromoPanels(c4, r1, AZP.Core.Media["Twitch"])
+	AZP.OptionsPanels:CreatePromoPanels(c1, r2, AZP.Core.Media["Insta"])
+	AZP.OptionsPanels:CreatePromoPanels(c2, r2, AZP.Core.Media["Twitter"])
+	AZP.OptionsPanels:CreatePromoPanels(c3, r2, AZP.Core.Media["GitHub"])
+	AZP.OptionsPanels:CreatePromoPanels(c4, r2, AZP.Core.Media["TouTube"])
+
+	local panelTitle = "AzerPUG's " .. panelName
 
 	if type(panelContent) == "string" then
 		optionFrame.placeholder = optionFrame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
 		optionFrame.placeholder:SetSize(optionFrame:GetWidth(), 500)
-		optionFrame.placeholder:SetPoint("TOP", 0, -50)
+		optionFrame.placeholder:SetPoint("TOP", 0, -150)
 		optionFrame.placeholder:SetJustifyH("LEFT")
 		optionFrame.placeholder:SetJustifyV("TOP")
 		optionFrame.placeholder:SetText(panelContent)
+		panelTitle = "\124cFFFF0000" .. panelTitle .. " not installed/loaded!\124r"
 	elseif type(panelContent) == "function" then
-		panelContent(optionFrame)
+		optionFrame.subFrame = CreateFrame("FRAME", nil, optionFrame)
+		optionFrame.subFrame:SetSize(optionFrame:GetWidth(), 500)
+		optionFrame.subFrame:SetPoint("TOP", 0, -150)
+		panelContent(optionFrame.subFrame)
+		optionFrame.subFrame:Show()
+		panelTitle = "\124cFF00FFFF" .. panelTitle .. "\124r"
 	end
+	optionFrame.title:SetText(panelTitle)
 end
 
 function AZP.OptionsPanels:Core()
@@ -182,56 +207,50 @@ end
 
 function AZP.OptionsPanels:ToolTips()
 	local panelName = "ToolTips"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent = "This is an AWESOME AddOn!"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:InterruptHelper()
 	local panelName = "Interrupt Helper"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Interrupt Helper creates an easy system to structure interrupts.\n" ..
 	"It is possible to put in people from your raid / party and share that list easily.\n" ..
 	"The AddOn automatically reorders the list after an interrupt for every one who has it installed.\n"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:InterfaceCompanion()
 	local panelName = "Interface Companion"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Interface Companion adds a nice companion to your interface.\n" ..
 	"Companion can be easily shown/hidden or moved though the option panel.\n" ..
 	"Do you want to be my friend?\n"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:PreparationCheckList()
 	local panelName = "Preparation CheckList"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Preparation CheckList helps the player prepare for Instances.\n" ..
 	"Within the options panel, you can select what to check for.\n" ..
 	"Flask, Food, Runes, Vantus and Scrolls are already implemented.\n" ..
 	"\n" ..
 	"The CheckList will then check how many of these items you have in your bags.\n"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:ReadyCheckEnhanced()
 	local panelName = "ReadyCheck Enhanced"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's ReadyCheck Enhanced helps the player see if they are actually ready.\n" ..
 	"The RCE frame will show Flask, Food, Rune, Int, Stam and Atk buffs.\n" ..
 	"Green/Red color for text will indicate presence of buff, including time left.\n"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:InstanceLeadership()
 	local panelName = "Instance Leadership"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Instance Leadership helps instance leaders in various ways.\n" ..
 	"Based on settings, the module adds buttons to the main addon's window.\n" ..
@@ -239,68 +258,61 @@ function AZP.OptionsPanels:InstanceLeadership()
 	"\n" ..
 	"The module can also add a button for combat logging and/or start automatic logging.\n" ..
 	"Automatic combat logging can be put on/off for different raids/dungeons."
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:EasierGreatVault()
 	local panelName = "Easier GreatVault"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's EasierGreatVault shows the user the current Great Vault reward-choices.\n" ..
 	"Eventually, it should also show what to do to upgrade/unlock the choices."
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:ManaManagement()
 	local panelName = "Mana Management"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Mana Management show a list of all healers with a mana bar to help you track your allies mana.\n" ..
 	"Additionally, it orders the mana bars based on mana % to make things easier to track."
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:MultipleReputationTracker()
 	local panelName = "Multiple Reputation Tracker"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Multiple Reputation Tracker provides the option to track several Factions at once.\n" ..
 	"The factions can easily be selected within the standard GUI.\n"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:ChatImprovements()
 	local panelName = "Chat Improvements"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Chat Improvements provides enhanced chat options.\n" ..
 	"In the options panel, a user defined nickname can be added behind the char name.\n"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:EfficientQuesting()
 	local panelName = "Efficient Questing"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Efficient Questing helps the player efficiently sart, do and end quests.\n" ..
 	"Currently: Pre-Selects the most valuable quest reward.\n"
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:LevelingStatistics()
 	local panelName = "Leveling Statistics"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent =
 	"AzerPUG's Leveling Statistics calculates the estimated time until the next level.\n" ..
 	"These stats are based on sessionTime and sessionXP."
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:UnLockables()
 	local panelName = "UnLockables"
-	local panelTitle = "\124cFFFF0000AzerPUG's " .. panelName .. " not installed/loaded!\124r\n"
 	local panelContent = "AzerPUG's UnLockables shows the required quest and/or questlines for certain unlockables."
-	return panelName, panelTitle, panelContent
+	return panelName, panelContent
 end
 
 function AZP.OptionsPanels:RemovePanel(PanelName)
