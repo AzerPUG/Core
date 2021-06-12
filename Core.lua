8b5a42325ef78b19da8a2f210cc19d94245c7de7
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2021-06-12 09:37:01 +0200

    Update supporting AZP Timed Encounters v3.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 83
+AZP.VersionControl["Core"] = 84
 if AZP.Core == nil then AZP.Core = {} end
 
 local dash = " - "
