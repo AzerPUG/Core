4a18b65e827f2481dff7b2e46dcd4ea672a987da
Arco Dielhof <arcodielhof@hotmail.com>
2021-07-14 00:50:33 +0200

    Update supporting the following AddOns: AzerPUG's Instance Leadership v23. AzerPUG's Timed Encounters v12. AzerPUG's ToolTips v34.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 95
+AZP.VersionControl["Core"] = 96
 if AZP.Core == nil then AZP.Core = {} end
 if AZP.Core.Events == nil then AZP.Core.Events = {} end
 
--- dataTables.lua
+++ dataTables.lua
@@ -3,11 +3,11 @@ if AZP.Core == nil then AZP.Core = {} end
 
 AZP.Core.AddOns =
 {
-    ["CR"]  = {["Name"] = "Core",                        ["Version"] = 95, ["Position"] =  2, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
+    ["CR"]  = {["Name"] = "Core",                        ["Version"] = 96, ["Position"] =  2, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["CI"]  = {["Name"] = "Chat Improvements",           ["Version"] = 29, ["Position"] =  3, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["EGV"] = {["Name"] = "Easier GreatVault",           ["Version"] = 10, ["Position"] =  4, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["EQ"]  = {["Name"] = "Efficient Questing",          ["Version"] = 13, ["Position"] =  5, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
-    ["IL"]  = {["Name"] = "Instance Leadership",         ["Version"] = 22, ["Position"] =  6, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
+    ["IL"]  = {["Name"] = "Instance Leadership",         ["Version"] = 23, ["Position"] =  6, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["IC"]  = {["Name"] = "Interface Companion",         ["Version"] =  7, ["Position"] =  7, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["IH"]  = {["Name"] = "Interrupt Helper",            ["Version"] = 19, ["Position"] =  8, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["LS"]  = {["Name"] = "Leveling Statistics",         ["Version"] = 13, ["Position"] =  9, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
@@ -15,8 +15,8 @@ AZP.Core.AddOns =
     ["MRT"] = {["Name"] = "Multiple Reputation Tracker", ["Version"] = 18, ["Position"] = 11, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["PCL"] = {["Name"] = "Preparation CheckList",       ["Version"] = 29, ["Position"] = 12, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["RCE"] = {["Name"] = "ReadyCheck Enhanced",         ["Version"] = 33, ["Position"] = 13, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
-    ["TE"]  = {["Name"] = "Timed Encounters",            ["Version"] = 10, ["Position"] = 14, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
-    ["TT"]  = {["Name"] = "ToolTips",                    ["Version"] = 33, ["Position"] = 15, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
+    ["TE"]  = {["Name"] = "Timed Encounters",            ["Version"] = 12, ["Position"] = 14, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
+    ["TT"]  = {["Name"] = "ToolTips",                    ["Version"] = 34, ["Position"] = 15, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
     ["UL"]  = {["Name"] = "UnLockables",                 ["Version"] =  9, ["Position"] = 16, ["Loaded"] = false, ["MainFrame"] = nil, ["Tab"] = nil},
 }
 
