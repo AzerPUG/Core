50c61c153c2a07b1232effafc18c9ed2cc6bfc4e
Arco Dielhof <arcodielhof@hotmail.com>
2021-08-23 18:45:49 +0200

    Update supporting Interface Companion v16.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 106
+AZP.VersionControl["Core"] = 107
 if AZP.Core == nil then AZP.Core = {} end
 if AZP.Core.Events == nil then AZP.Core.Events = {} end
 
