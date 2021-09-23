68b788d64b7d96fd3b536ce8d36ce3a85e233bd5
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2021-09-23 23:55:11 +0200

    Update supporting AzerPUG's ToolTips.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 113
+AZP.VersionControl["Core"] = 114
 if AZP.Core == nil then AZP.Core = {} end
 if AZP.Core.Events == nil then AZP.Core.Events = {} end
 
