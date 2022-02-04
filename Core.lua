38b48af39f34e7446c3048d4eb602ff3b63e9472
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2022-02-04 20:29:16 +0100

    Update supporting AzerPUG's ReadyCheck Enhanced v47.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 126
+AZP.VersionControl["Core"] = 127
 if AZP.Core == nil then AZP.Core = {} end
 if AZP.Core.Events == nil then AZP.Core.Events = {} end
 
