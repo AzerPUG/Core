cfd9dabbe8d4304e5394a06e0c35bff5684535f4
Arco Dielhof <arcodielhof@hotmail.com>
2022-06-05 22:08:32 +0200

    Update supporting AzerPUG's Interface Companion v41.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Core"] = 143
+AZP.VersionControl["Core"] = 144
 if AZP.Core == nil then AZP.Core = {} end
 if AZP.Core.Events == nil then AZP.Core.Events = {} end
 
